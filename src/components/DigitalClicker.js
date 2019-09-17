253b4c08aff697e212a9e3d7419b3121c7719e61
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-16 22:23:47 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,35 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    //this was undefined in debugger, but works in console.log
+    // console.log(this.state)
+    this.setState({
+      timesClicked: this.state.timesClicked + 1 //++ caused issue
+    })
+  }
+
+  // handleClick = () => {
+  //   let timesClicked = this.state.timesClicked + 1
+  //   this.setState({
+  //     timesClicked: timesClicked
+  //   })
+  // }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.timesClicked}
+      </button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,59 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+export default class YouTubeDebugger extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitrateClick = () => {
+    this.setState({
+      ...this.state,
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResolutionClick = () => {
+    this.setState({
+      ...this.state,
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: '720p'
+        }
+      }
+    })
+
+  }
+//Syntax error: Adjacent JSX elements must be wrapped in an enclosing tag
+//Can only return one jsx element from a render
+//but that element can have children
+  render() {
+    return (
+      <div>
+      <button
+        className='bitrate'
+        onClick={this.handleBitrateClick}
+      />
+      <button
+        className='resolution'
+        onClick={this.handleResolutionClick}
+      />
+    </div>
+    )
+  }
+}
